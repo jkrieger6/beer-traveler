7cc930c035111a90c701ed6d8ee3f71fc06d6503
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-04-06 20:55:33 -0400

    fixing issues with reset of data into the cards and removing duplicate code. formatted with prettier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,53 @@
-var userInputEl = document.querySelector('#searchCriteria');
+var userInputEl = document.querySelector("#searchCriteria");
 var APIKey = "5b2b7b9a047f3cbe2fa1edd5d1203608";
 
 var searchBtnEl = document.querySelector("#searchBtn");
 searchBtnEl.addEventListener("click", addResult);
 
-// $(document).ready(function(){
-//     $("#searchBtn").click(function (){
-//         var userInput = $(userInputEl).val();
-//         $("#weatherPane").attr("style", "display:inline-block");
-//         searchCity(userInput);
-//     });
-// });
-
 function addResult(event) {
-    let userInputEl = document.querySelector('#searchCriteria');
-    event.preventDefault();
-    let userInputEl = document.querySelector('#searchCriteria');
-    var cityInput = userInputEl.value;
-    console.log(cityInput);
+  event.preventDefault();
+  let userInputEl = document.querySelector("#searchCriteria");
+  var cityInput = userInputEl.value;
+  console.log(cityInput);
 
-    // $("#current-weather").attr("style", "display:inline-block");
-    console.log(cityInput);
-    searchCity(cityInput);
-  }
+  // $("#current-weather").attr("style", "display:inline-block");
+  console.log(cityInput);
+  searchCity(cityInput);
+}
 
-  // function to get lat, lon of city
+// function to get lat, lon of city
 function searchCity(userInput) {
-  $("#weatherPane").empty();
-    // api search function goes here
-    var geoUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" +
+  document.querySelector("#weatherPane").innerHTML = "";
+  // api search function goes here
+  var geoUrl =
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
     userInput +
     "&limit=5&appid=" +
     APIKey;
-    fetch(geoUrl)
+  fetch(geoUrl)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
-        var cityData = data[0];
-        console.log(cityData);
-        var geoLat = cityData.lat;
-        var geoLon = cityData.lon;
-        getWeatherData(geoLat, geoLon, cityData);
-        setMap(geoLon, geoLat);
-    })
-    console.log("Your city is: " + userInput);
+      var cityData = data[0];
+      console.log(cityData);
+      var geoLat = cityData.lat;
+      var geoLon = cityData.lon;
+      getWeatherData(geoLat, geoLon, cityData);
+      setMap(geoLon, geoLat);
+    });
+  console.log("Your city is: " + userInput);
 }
 
 // function to get current weather for searched city
 function currentWeather(weatherObj) {
-    // var dateTime = weatherObj.dt_txt;
-    var tempData = weatherObj.main.temp;
-    var windSpeedData = weatherObj.wind.speed;
-    var currentConditionsData = weatherObj.weather[0].description;
+  // var dateTime = weatherObj.dt_txt;
+  var tempData = weatherObj.main.temp;
+  var windSpeedData = weatherObj.wind.speed;
+  var currentConditionsData = weatherObj.weather[0].description;
   var weatherIcon = weatherObj.weather[0].icon;
   imgSrc = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
-  var cityEl =$("#weatherPane");
+  var cityEl = $("#weatherPane");
   cityEl.append($("<div>").text(" Temp: " + tempData + "F"));
   cityEl.append($("<div>").text(" Wind Speed: " + windSpeedData + "mph"));
   cityEl.append($("<div>").text(" Conditions: " + currentConditionsData));
@@ -64,49 +56,57 @@ function currentWeather(weatherObj) {
 
 // function to get forecasted weather for searched city from earlier called API
 function getForecast(arrayOfWeatherObjs) {
-    var list = $("#weatherPane");
-    console.log(arrayOfWeatherObjs);
-    for (let i = 0; i < arrayOfWeatherObjs.length; i++) {
-        var obj = arrayOfWeatherObjs[i];
-        var dayTimeDisplay = obj.dt_txt;
-        if(obj.dt_txt.includes("12:00:00")) {
-            var forecastListItems = $("<li>");
-            var forecastTempData = Math.floor(obj.main.temp);
-            var forecastWindSpeedData = Math.floor(obj.wind.speed);
-            var forecastCoonditions = obj.weather[0].description;
-            var forecastWeatherIcon = obj.weather[0].icon;
-            imgSrc = "https://openweathermap.org/img/wn/" + forecastWeatherIcon + ".png";
-            forecastListItems.append($("<div>").text(dayTimeDisplay));
-            forecastListItems.append($("<div>").text(" Temp: " + forecastTempData + "F"));
-            forecastListItems.append($("<div>").text(" Wind Speed: " + forecastWindSpeedData + "mph"));
-            forecastListItems.append($("<div>").text(" Condtions: " + forecastCoonditions));
-            forecastListItems.append($("<img>").attr("src", imgSrc));
-            list.append(forecastListItems);
-        }
+  var list = $("#weatherPane");
+  console.log(arrayOfWeatherObjs);
+  for (let i = 0; i < arrayOfWeatherObjs.length; i++) {
+    var obj = arrayOfWeatherObjs[i];
+    var dayTimeDisplay = obj.dt_txt;
+    if (obj.dt_txt.includes("12:00:00")) {
+      var forecastListItems = $("<li>");
+      var forecastTempData = Math.floor(obj.main.temp);
+      var forecastWindSpeedData = Math.floor(obj.wind.speed);
+      var forecastCoonditions = obj.weather[0].description;
+      var forecastWeatherIcon = obj.weather[0].icon;
+      imgSrc =
+        "https://openweathermap.org/img/wn/" + forecastWeatherIcon + ".png";
+      forecastListItems.append($("<div>").text(dayTimeDisplay));
+      forecastListItems.append(
+        $("<div>").text(" Temp: " + forecastTempData + "F")
+      );
+      forecastListItems.append(
+        $("<div>").text(" Wind Speed: " + forecastWindSpeedData + "mph")
+      );
+      forecastListItems.append(
+        $("<div>").text(" Condtions: " + forecastCoonditions)
+      );
+      forecastListItems.append($("<img>").attr("src", imgSrc));
+      list.append(forecastListItems);
     }
+  }
 }
 
 // calls Open weather API to get weather
 function getWeatherData(lat, lon) {
-    $("#weatherPane").addClass("list-group");
-    $("#weatherPane").append($("<ol>"));
-    $("#weatherPane").attr("style", "display:inline-block");
-    
-    var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+  $("#weatherPane").addClass("list-group");
+  $("#weatherPane").append($("<ol>"));
+  $("#weatherPane").attr("style", "display:inline-block");
+
+  var weatherUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
     lat +
     "&lon=" +
     lon +
     "&units=imperial&appid=" +
     APIKey;
 
-    fetch(weatherUrl)
+  fetch(weatherUrl)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
       currentWeather(data.list[0]);
-        getForecast(data.list);
-    })
+      getForecast(data.list);
+    });
 }
 var searchEL = document.querySelector("#searchBtn");
 
@@ -114,38 +114,38 @@ searchEL.addEventListener("click", searchBreweries);
 
 //search function for breweries
 function searchBreweries() {
-    userInputEl = document.querySelector("#searchCriteria").value;
-    const city = document.getElementById("myCity").value;
+  userInputEl = document.querySelector("#searchCriteria").value;
+  document.querySelector("#beerGlass").innerHTML = "";
 
-    const endpoint = "https://api.openbrewerydb.org/breweries?by_city="+userInputEl+"&per_page=5";
-    fetch(endpoint)
-      .then(response => response.json())
-      .then(breweries => {
-        //const breweriesList = document.getElementById("beerGlass");
-        for (const brewery of breweries) {
-          console.log(brewery);
-        }
-      })
-      .catch(error => console.error(error));
+  const endpoint =
+    "https://api.openbrewerydb.org/breweries?by_city=" +
+    userInputEl +
+    "&per_page=5";
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((breweries) => {
+      //const breweriesList = document.getElementById("beerGlass");
+      for (const brewery of breweries) {
+        console.log(brewery);
+      }
+    })
+    .catch((error) => console.error(error));
+}
 
-    }
-    
 function brewInfo(breweryObj) {
+  var brewName = breweryObj.name;
+  var brewAddress = breweryObj.address_1;
+  var brewType = breweryObj.brewery_type;
+  var brewLink = breweryObj.website_url;
 
-    var brewName = breweryObj.name;
-    var brewAddress = breweryObj.address_1;
-    var brewType = breweryObj.brewery_type;
-    var brewLink = breweryObj.website_url;
+  var breweryEl = $("#beerGlass");
+  breweryEl.append($("<div>").text(brewName));
+  breweryEl.append($("<div>").text(brewAddress));
+  breweryEl.append($("<div>").text(brewType));
+  breweryEl.append($("<a>").attr("href=", brewLink));
 
-    
-    var breweryEl =$("#beerGlass");
-    breweryEl.append($("<div>").text(brewName));
-    breweryEl.append($("<div>").text(brewAddress));
-    breweryEl.append($("<div>").text(brewType));
-    breweryEl.append($("<a>").attr("href=", brewLink));
-        
-        //$("#beerGlass").addclass("list-group");
-        //$("#beerGlass").append($("<ol>"));
+  //$("#beerGlass").addclass("list-group");
+  //$("#beerGlass").append($("<ol>"));
 }
 
 // define the OpenLayers map object
@@ -154,7 +154,7 @@ var map = new ol.Map("#myCity");
 // define the search box element and add an event listener
 var searchBox = userInputEl.value;
 var searchBtn = document.querySelector("#searchBtn");
-searchBtn.addEventListener("click", function() {
+searchBtn.addEventListener("click", function () {
   var city = searchBox.value;
   //getCityLatLng(city);
 });
@@ -163,16 +163,16 @@ function setMap(lon, lat) {
   console.log(ol);
   document.querySelector("#myCity").innerHTML = "";
   map = new ol.Map({
-    target: 'myCity',
+    target: "myCity",
     layers: [
       new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
+        source: new ol.source.OSM(),
+      }),
     ],
-       view: new ol.View({
-        center: ol.proj.fromLonLat([lon, lat]),
-        zoom: 10
-     })
+    view: new ol.View({
+      center: ol.proj.fromLonLat([lon, lat]),
+      zoom: 10,
+    }),
   });
 }
 
